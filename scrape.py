e700b5b15dfb86660f377f4323002187d28f0833
Toni Melisma <59022391+tonimelisma@users.noreply.github.com>
2023-05-25 19:53:27 -0700

    pagination

--- scrape.py
+++ scrape.py
@@ -16,127 +16,150 @@ my_leadid = "5a92786c-531d-4fd2-9954-470044207fc6"
 my_model = "sienna"
 
 
+def wait():
+    sleep(randint(12, 36))
+
+
 def fetchvehicles(zipcode, seriescodes, leadid):
     url = "https://api.search-inventory.toyota.com/graphql"
 
-    query = """      query {
-          locateVehiclesByZip(zipCode: "%s", brand: "TOYOTA", pageNo: 1, pageSize: 250, seriesCodes: "%s", distance: 500, leadid: "%s") {
-            pagination {
-              pageNo
-              pageSize
-              totalPages
-              totalRecords
-            }
-            vehicleSummary {
-              vin
-              stockNum
-              brand
-              marketingSeries
-              year
-              isTempVin
-              dealerCd
-              dealerCategory
-              distributorCd
-              holdStatus
-              weightRating
-              isPreSold
-              dealerMarketingName
-              dealerWebsite
-              isSmartPath
-              distance
-              isUnlockPriceDealer
-              transmission {
-                transmissionType
-              }
-              price {
-                advertizedPrice
-                nonSpAdvertizedPrice
-                totalMsrp
-                sellingPrice
-                dph
-                dioTotalMsrp
-                dioTotalDealerSellingPrice
-                dealerCashApplied
-                baseMsrp
-              }
-              options {
-                optionCd
-                marketingName
-                marketingLongName
-                optionType
-                packageInd
-              }
-              mpg {
-                city
-                highway
-                combined
-              }
-              model {
-                modelCd
-                marketingName
-                marketingTitle
-              }
-              media {
-                type
-                href
-                imageTag
-                source
-              }
-              intColor {
-                colorCd
-                colorSwatch
-                marketingName
-                nvsName
-                colorFamilies
-              }
-              extColor {
-                colorCd
-                colorSwatch
-                marketingName
-                colorHexCd
-                nvsName
-                colorFamilies
-              }
-              eta {
-                currFromDate
-                currToDate
-              }
-              engine {
-                engineCd
-                name
-              }
-              drivetrain {
-                code
-                title
-                bulletlist
-              }
-              family
-              cab {
-                code
-                title
-                bulletlist
-              }
-              bed {
-                code
-                title
-                bulletlist
-              }
-            }
-          }
-        }""" % (zipcode, seriescodes, leadid)
+    page_no = 1
+    has_more_pages = True
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
-    }
+    vehicles = []
 
-    r = requests.post(url=url, json={"query": query}, headers=headers)
-    data = r.json()
+    while has_more_pages:
+        query = f"""
+query {{
+  locateVehiclesByZip(zipCode: "{zipcode}", brand: "TOYOTA", pageNo: {page_no}, pageSize: 250, seriesCodes: "{seriescodes}", distance: 500, leadid: "{leadid}") {{
+    pagination {{
+      pageNo
+      pageSize
+      totalPages
+      totalRecords
+    }}
+    vehicleSummary {{
+      vin
+      stockNum
+      brand
+      marketingSeries
+      year
+      isTempVin
+      dealerCd
+      dealerCategory
+      distributorCd
+      holdStatus
+      weightRating
+      isPreSold
+      dealerMarketingName
+      dealerWebsite
+      isSmartPath
+      distance
+      isUnlockPriceDealer
+      transmission {{
+        transmissionType
+      }}
+      price {{
+        advertizedPrice
+        nonSpAdvertizedPrice
+        totalMsrp
+        sellingPrice
+        dph
+        dioTotalMsrp
+        dioTotalDealerSellingPrice
+        dealerCashApplied
+        baseMsrp
+      }}
+      options {{
+        optionCd
+        marketingName
+        marketingLongName
+        optionType
+        packageInd
+      }}
+      mpg {{
+        city
+        highway
+        combined
+      }}
+      model {{
+        modelCd
+        marketingName
+        marketingTitle
+      }}
+      media {{
+        type
+        href
+        imageTag
+        source
+      }}
+      intColor {{
+        colorCd
+        colorSwatch
+        marketingName
+        nvsName
+        colorFamilies
+      }}
+      extColor {{
+        colorCd
+        colorSwatch
+        marketingName
+        colorHexCd
+        nvsName
+        colorFamilies
+      }}
+      eta {{
+        currFromDate
+        currToDate
+      }}
+      engine {{
+        engineCd
+        name
+      }}
+      drivetrain {{
+        code
+        title
+        bulletlist
+      }}
+      family
+      cab {{
+        code
+        title
+        bulletlist
+      }}
+      bed {{
+        code
+        title
+        bulletlist
+      }}
+    }}
+  }}
+}}
+"""
 
-    if r.status_code != 200:
-        print("error: [", r.status_code, "] ", r.reason)
-        exit(1)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        }
 
-    vehicles = data['data']['locateVehiclesByZip']['vehicleSummary']
+        res = requests.post(url=url, json={"query": query}, headers=headers)
+        print("page ", page_no, ", status: ", res.status_code, " ", res.reason)
+        if res.status_code != 200:
+            print("error: [", res.status_code, "] ", res.reason)
+            exit(1)
+
+        data = res.json()
+
+        vehicles.append(data['data']['locateVehiclesByZip']['vehicleSummary'])
+
+        page_no = data['data']['locateVehiclesByZip']['pagination']['pageNo']
+        total_pages = data['data']['locateVehiclesByZip']['pagination']['totalPages']
+
+        if page_no >= total_pages:
+            has_more_pages = False
+            print("no more pages")
+        else:
+            page_no += 1
+            wait()
     return vehicles
 
 
@@ -144,7 +167,7 @@ vehicles = {}
 for zipcode in zipcodes:
     vehicles[zipcode] = fetchvehicles(
         zipcode, my_model, my_leadid)
-    sleep(randint(120, 360))
+    wait()
 
 for zipcode in vehicles:
     date = datetime.datetime.now().date()
